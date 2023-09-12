87b119a33b716c7909fd048fe97440680137947a
Richard Torres(JunkLab) <richard.ms.torres@gmail.com>
2023-09-12 17:24:43 -0300

    ajustes

--- Calculadora_IMC.py
+++ Calculadora_IMC.py
@@ -10,30 +10,36 @@ def Calculo(peso, altura):
     IMC = peso / (altura ** 2)
     return IMC
 def Mostragem(IMC):
-  if IMC < 18.50:
-    return "Abaixo do peso"
+  if IMC < 15.99:
+    return "Muito abaixo do peso."
+  elif 16.0 <= IMC <18.49:
+    return "Abaixo do peso."
   elif 18.50 <= IMC < 24.99:
-    return "Regular"
+    return "Regular."
   elif 24.99 <= IMC < 29.99:
-    return "Sobrepeso"
+    return "Sobrepeso."
   elif 29.99 <= IMC < 34.99:
-    return "Obesidade I"
+    return "Obesidade I."
   elif 34.99 <= IMC < 39.99:
-    return "Obesidade II"
+    return "Obesidade II."
   else:
-    return "Obesidade III (obesidade mórbida)"
-  
-# Interação do algoritmo
+    return "Obesidade III (obesidade mórbida)."
+   
+# Interação do algoritmo 
 
 print()
 print("Olá, você está no aplicativo de cálculo de IMC (Índice de massa corporal) \n")
-altura = float(input("Digite sua altura em metros 'M' ex: 0.00 : \n"))
 peso = float(input("Digite seu peso em quilogramas 'KG' ex: 00.00 : \n"))
+altura = float(input("Digite sua altura em metros 'M' ex: 0.00 : \n"))
 imc = Calculo(peso, altura)
 resultado = Mostragem(imc)
+
+#Saida de dados
+
 print()
-print(f"O resultado do IMC é: {imc:.2f} \n")
-print(f"Estado atual: {resultado} \n")
+print(f"Peso digitado: {peso:.2f}Kg\n" f"Altura digitada: {altura:.2f}M\n")
+print(f"Seu IMC é: {imc:.2f} \n")
+print(f"Classificação do IMC: {resultado} \n")
 print("Operação finalizada, Obrigado! \n")
 
 # Fim
